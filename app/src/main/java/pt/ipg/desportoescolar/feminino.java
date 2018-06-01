fbb4b4d5f0cdcc5db46b756f85edb6d380819a7e
Pedro Silva <josuepedro08@gmail.com>
2018-06-01 12:06:22 +0100

    added 2 more activities (masculino and feminino)

--- app/src/main/java/pt/ipg/desportoescolar/feminino.java
+++ app/src/main/java/pt/ipg/desportoescolar/feminino.java
@@ -1,19 +1,13 @@
 package pt.ipg.desportoescolar;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    
-
+public class feminino extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_feminino);
     }
 }
