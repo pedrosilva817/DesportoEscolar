b2c435160f57c8f16d1e1d1350298be053d49192
Pedro Silva <josuepedro08@gmail.com>
2018-06-07 14:52:55 +0100

    Added CRUD funtionality to the desportos table

--- app/src/main/java/pt/ipg/desportoescolar/DbTableDesportos.java
+++ app/src/main/java/pt/ipg/desportoescolar/DbTableDesportos.java
@@ -1,7 +1,7 @@
 package pt.ipg.desportoescolar;
 
 import android.content.ContentValues;
-
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
@@ -13,6 +13,7 @@ public class DbTableDesportos implements BaseColumns {
 
     public static final String TABLE_NAME = "desportos";
     private static final String FIELD_NAME = "nome";
+    private static final String FIELD_DATE = "data";
 
     private SQLiteDatabase db;
 
@@ -46,4 +47,16 @@ public class DbTableDesportos implements BaseColumns {
     public long insert(ContentValues values) {
         return db.insert(TABLE_NAME, null, values);
     }
+
+    public int update(ContentValues values, String whereClause, String[] whereArgs) {
+        return db.update(TABLE_NAME, values, whereClause, whereArgs);
+    }
+
+    public int delete(String whereClause, String[] whereArgs) {
+        return db.delete(TABLE_NAME, whereClause, whereArgs);
+    }
+    public Cursor query (String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy) {
+        return db.query(TABLE_NAME, columns, selection, selectionArgs, groupBy, having, orderBy);
+    }
+
 }
