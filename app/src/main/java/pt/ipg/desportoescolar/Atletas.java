a10a83ba71069861e2dd99026710259808444974
Pedro Silva <josuepedro08@gmail.com>
2018-06-08 12:39:37 +0100

    Deleted and added a new class (-resultados +atletas)

---
+++ app/src/main/java/pt/ipg/desportoescolar/Atletas.java
@@ -0,0 +1,40 @@
+package pt.ipg.desportoescolar;
+
+public class Atletas {
+    private int id;
+    private String name;
+    private int age;
+    private String course;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public void setPrice(int age) {
+        this.age = age;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
