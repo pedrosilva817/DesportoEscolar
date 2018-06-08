7990ebc939601426bcfba12cb26910f925d029ac
Pedro Silva <josuepedro08@gmail.com>
2018-06-08 12:22:50 +0100

    Delete and Added a table (DbTableAtletas)

--- app/src/main/java/pt/ipg/desportoescolar/DbTableAtletas.java
+++ app/src/main/java/pt/ipg/desportoescolar/DbTableAtletas.java
@@ -9,13 +9,17 @@ import android.database.Cursor;
  * Created by Pedro on 31/06/2018.
  */
 
-public class DbTableResultados implements BaseColumns {
-    public static final String TABLE_NAME = "resultados";
-    private static final String FIELD_TYPE = "tipo";
+public class DbTableAtletas implements BaseColumns {
+    public static final String TABLE_NAME = "jogadores";
+    private static final String FIELD_NAME = "tipo";
+    private static final String FIELD_AGE = "idade";
+    private static final String FIELD_COURSE = "curso";
+
+    public static final String [] ALL_COLUMNS = new String[] { _ID, FIELD_NAME, FIELD_AGE, FIELD_COURSE };
 
     private SQLiteDatabase db;
 
-    public DbTableResultados(SQLiteDatabase db) {
+    public DbTableAtletas(SQLiteDatabase db) {
         this.db = db;
     }
 
@@ -23,16 +27,20 @@ public class DbTableResultados implements BaseColumns {
         db.execSQL(
                 "CREATE TABLE " + TABLE_NAME + " (" +
                         _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                        FIELD_TYPE + " TEXT NOT NULL" +
+                        FIELD_NAME + " TEXT NOT NULL, " +
+                        FIELD_AGE + " REAL " +
+                        FIELD_COURSE + " TEXT NOT NULL" +
                         ")"
         );
     }
 
-    public static ContentValues getContentValues(Resultados resultados) {
+    public static ContentValues getContentValues(Atleta atleta) {
         ContentValues values = new ContentValues();
 
-        values.put(_ID, resultados.getId());
-        values.put(FIELD_TYPE, resultados.getName());
+        values.put(_ID, atleta.getId());
+        values.put(FIELD_NAME, atleta.getName());
+        values.put(FIELD_AGE, atleta.getAge());
+        values.put(FIELD_COURSE, atleta.getCourse());
 
         return values;
     }
@@ -51,6 +59,6 @@ public class DbTableResultados implements BaseColumns {
 
     public Cursor query (String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy) {
         return db.query(TABLE_NAME, columns, selection, selectionArgs, groupBy, having, orderBy);
-        }
+    }
 
 }
