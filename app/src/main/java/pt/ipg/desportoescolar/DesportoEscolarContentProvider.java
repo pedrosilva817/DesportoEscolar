e8962fca8978ee83afa117c171c5128faf95a19f
Pedro Silva <josuepedro08@gmail.com>
2018-06-14 20:51:49 +0100

    Implemented query (desporto escolar content provider)

--- app/src/main/java/pt/ipg/desportoescolar/DesportoEscolarContentProvider.java
+++ app/src/main/java/pt/ipg/desportoescolar/DesportoEscolarContentProvider.java
@@ -2,8 +2,13 @@ package pt.ipg.desportoescolar;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.ContentObserver;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
@@ -11,7 +16,25 @@ import android.support.annotation.Nullable;
  * Created by Pedro on 14/06/2018.
  */
 public class DesportoEscolarContentProvider extends ContentProvider {
+    private static final int ATLETAS = 100;
+    private static final int ATLETAS_ID = 101;
+    private static final int DESPORTOS = 200;
+    private static final int DESPORTOS_ID = 201;
+
     DbDesportoEscolarOpenHelper desportoEscolarOpenHelper;
+
+
+    private static UriMatcher getDesportoEscolarUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI("pt.ipg.desportoescolar", "atletas", ATLETAS);
+        uriMatcher.addURI("pt.ipg.desportoescolar", "atletas/#", ATLETAS_ID);
+
+        uriMatcher.addURI("pt.ipg.desportoescolar", "desportos", DESPORTOS);
+        uriMatcher.addURI("pt.ipg.desportoescolar", "desportos/#", DESPORTOS_ID);
+
+        return uriMatcher;
+    }
 
     @Override
     public boolean onCreate() {
@@ -25,6 +48,31 @@ public class DesportoEscolarContentProvider extends ContentProvider {
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
         return null;
+
+        SQLiteDatabase db = desportoEscolarOpenHelper.getReadableDatabase();
+
+        String id = uri.getLastPathSegment();
+
+        UriMatcher matcher = getDesportoEscolarUriMatcher();
+
+        switch (matcher.match(uri)) {
+            case ATLETAS:
+                return new DbTableAtletas(db).query(projection, selection, selectionArgs, null, null, sortOrder);
+
+            case DESPORTOS:
+                return new DbTableDesportos(db).query(projection, selection, selectionArgs, null, null, sortOrder);
+
+            case ATLETAS_ID:
+                return new DbTableAtletas(db).query(projection, DbTableAtletas._ID + "=?", new String[] { id }, null, null, null);
+
+            case DESPORTOS_ID:
+                return new DbTableDesportos(db).query(projection, DbTableDesportos._ID + "=?", new String[] { id }, null, null, null);
+
+            default:
+                throw new UnsupportedOperationException("Invalid URI: " + uri);
+        }
+    }
+
     }
 
     @Nullable
