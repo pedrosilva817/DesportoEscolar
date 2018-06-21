f629a3ea5325fb282e729c69a3cf026b2d72c7bb
pedrosilva817 <39406947+pedrosilva817@users.noreply.github.com>
2018-06-21 22:47:28 +0100

    Added DesportoEscolar database seed (for no production environments) (not finished)

--- app/src/main/java/pt/ipg/desportoescolar/DbDesportoEscolarOpenHelper.java
+++ app/src/main/java/pt/ipg/desportoescolar/DbDesportoEscolarOpenHelper.java
@@ -6,6 +6,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DbDesportoEscolarOpenHelper extends SQLiteOpenHelper {
 
+    private static final boolean PRODUCTION = false;
+
     public static final String DATABASE_NAME = "desportoescolar.db";
     private static final int DATABASE_VERSION = 1;
 
@@ -14,8 +16,54 @@ public class DbDesportoEscolarOpenHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    @Override
+   public void onCreate(SQLiteDatabase db) {
+               DbTableDesportos dbTableDesportos = new DbTableDesportos(db);
+               dbTableDesportos.create();
 
+               DbTableAtletas dbTableAtletas = new DbTableAtletas(db);
+               dbTableAtletas.create();
 
+        if (!PRODUCTION) {
+            seed(db);
+        }
+    }
+    private void seed(SQLiteDatabase db) {
+        DbTableCategories dbTableCategories = new DbTableCategories(db);
+
+        Category category = new Category();
+        category.setName("Drama");
+        int idCategoryDrama = (int) dbTableCategories.insert(DbTableCategories.getContentValues(category));
+
+        category = new Category();
+        category.setName("Romance");
+        int idCategoryRomance = (int) dbTableCategories.insert(DbTableCategories.getContentValues(category));
+
+        category = new Category();
+        category.setName("Comedy");
+        dbTableCategories.insert(DbTableCategories.getContentValues(category));
+
+        DbTableBooks dbTableBooks = new DbTableBooks(db);
+
+        Book book = new Book();
+        book.setTitle("A midsummer night's dream");
+        book.setIdCategory(idCategoryRomance);
+        book.setPrice(9.99);
+        dbTableBooks.insert(DbTableBooks.getContentValues(book));
+
+        book = new Book();
+        book.setTitle("Hamlet");
+        book.setIdCategory(idCategoryDrama);
+        book.setPrice(11.99);
+        dbTableBooks.insert(DbTableBooks.getContentValues(book));
+
+        book = new Book();
+        book.setTitle("Macbeth");
+        book.setIdCategory(idCategoryDrama);
+        book.setPrice(5.99);
+        dbTableBooks.insert(DbTableBooks.getContentValues(book));
+    }
+    
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
@@ -23,7 +71,7 @@ public class DbDesportoEscolarOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     }
 }
 
