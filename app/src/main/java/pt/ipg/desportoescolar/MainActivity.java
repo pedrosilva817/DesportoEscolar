82c5af54216ab032aecb1f6fc42c9abb6fdd1bc4
pedrosilva817 <39406947+pedrosilva817@users.noreply.github.com>
2018-07-05 11:42:27 +0100

    fixing some layout issues

--- app/src/main/java/pt/ipg/desportoescolar/MainActivity.java
+++ app/src/main/java/pt/ipg/desportoescolar/MainActivity.java
@@ -1,23 +1,24 @@
 package pt.ipg.desportoescolar;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CursorAdapter;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
-
     private static final int DESPORTOESCOLAR_CURSOR_LOADER_ID = 0;
     public static final String DESPORTOESCOLAR_ID = "DESPORTOESCOLAR_ID";
 
@@ -40,14 +41,14 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         atletasCursorAdapter.setViewHolderClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editAtelta();
+                editAtleta();
             }
         });
 
         getSupportLoaderManager().initLoader(DESPORTOESCOLAR_CURSOR_LOADER_ID, null, this);
     }
 
-    private void editAtelta() {
+    private void editAtleta() {
         int id = atletasCursorAdapter.getlastAtletaClicked();
 
         Intent intent = new Intent(this, EditAtletaActivity.class);
@@ -65,19 +66,14 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -85,11 +81,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         return super.onOptionsItemSelected(item);
     }
 
+
+
     @NonNull
-    //@Override
+    @Override
     public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
         if (id == DESPORTOESCOLAR_CURSOR_LOADER_ID) {
-            return new CursorLoader(this, DesportoEscolarContentProvider.ATLETAS_URI, DbTableAtletas.ALL_COLUMNS,
+            return new CursorLoader(this, DesportoEscolarContentProvider.DESPORTOS_URI, DbTableAtletas.ALL_COLUMNS,
                     null, null, null);
         }
 
@@ -108,5 +106,3 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     }
 
 }
-
-
